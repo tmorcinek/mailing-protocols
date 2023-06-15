815156da060c5cda04f3896732b362ff33eb9b5a
Bartosz Przechera <bartoszprzechera@gmail.com>
2023-06-15 19:39:30 +0200

    IMAP v2

--- src/main/kotlin/IMAPClient.kt
+++ src/main/kotlin/IMAPClient.kt
@@ -11,10 +11,11 @@ fun main(args: Array<String>) {
     imapClient.noop()
     imapClient.login("bprzechera@student.agh.edu.pl", "")
     imapClient.list()
-//    imapClient.select("INBOX")
-//    imapClient.fetch("530")
-    imapClient.select("Sent")
-    imapClient.get_mail("Sent","50")
+
+    var last_id=imapClient.select("INBOX")
+    val email:Email=imapClient.get_mail("INBOX",last_id)
+    println(email)
+
 }
 data class Email(var from:String,
                  var to:String,
@@ -72,20 +73,26 @@ class IMAPClient(host: String, port: Int) {
 //        sendMessage("CAPABILITY")
 //        readServerMessage()
     }
-    fun select(name:String,doPrint: Boolean =true){
+    fun select(name:String,doPrint: Boolean =true):String{
+        var count:String="NOT FOUND"
+        val countRegex = "\\* [0-9]+ EXISTS".toRegex()
         val messageId=sendMessage("SELECT $name",doPrint)
         var lastMessage:String=""
         while (!(lastMessage.contains(messageId, ignoreCase = true)&& lastMessage.contains("SELECT completed", ignoreCase = true)))  {
             lastMessage=readServerMessage(doPrint)
+            val regex_temp=countRegex.find(lastMessage)
+            if (regex_temp!=null){
+                count=regex_temp.value.replace("EXISTS","").replace("*","").replace(" ","")
+            }
         }
+        return count
     }
-    fun get_mail(box_name:String,mail_id:String):Email{
+    fun process_email(body_text:String,header_from:String="",header_to:String="",header_date:String=""):Email{
         val fromRegex = """From:.*\n""".toRegex()
         val toRegex = """To:.*\n""".toRegex()
         val dateRegex = """Date:.*\n""".toRegex()
-        val boundaryRegex = "boundary=\".*\"\n".toRegex()
+        val boundaryRegex = "--.*=_.*\n".toRegex()
         val contentTypeRegex="Content-Type:.*;".toRegex()
-        val fileBoundary_regex="--=_.*\n".toRegex()
         val contentEncodingRegex="Content-Transfer-Encoding:.*\n".toRegex()
         val contentNameRegex="name=\".*\"\n".toRegex()
         var from:String=""
@@ -94,48 +101,64 @@ class IMAPClient(host: String, port: Int) {
         var text:String=""
         var html:String=""
         var filelist:ArrayList<String> = ArrayList<String>()
-        select(box_name,false)
 
-        var temp=fromRegex.find(fetch(mail_id,"BODY[HEADER.FIELDS (FROM)]",false))
-        if (temp != null)
-            from=temp.value.replace("From: ","").replace("\n","")
-        temp=null
-        temp=toRegex.find(fetch(mail_id,"BODY[HEADER.FIELDS (TO)]",false))
-        if (temp != null)
-            to=temp.value.replace("To: ","").replace("\n","")
-        temp=null
-        temp=dateRegex.find(fetch(mail_id,"BODY[HEADER.FIELDS (DATE)]",false))
-        if (temp != null)
-            date=temp.value.replace("To: ","").replace("\n","")
-        temp=null
-        var temp_text=fetch(mail_id,"RFC822.TEXT",false)
-        var boundary_temp=boundaryRegex.find(temp_text)
+        var header_from_temp=fromRegex.find(header_from)
+        if (header_from_temp != null)
+            from=header_from_temp.value.replace("From: ","").replace("\n","")
+        var header_to_temp=toRegex.find(header_to)
+        if (header_to_temp != null)
+            to=header_to_temp.value.replace("To: ","").replace("\n","")
+        var header_date_temp=dateRegex.find(header_date)
+        if (header_date_temp != null)
+            date=header_date_temp.value.replace("Date: ","").replace("\n","")
+
+
+        var boundary_temp=boundaryRegex.find(body_text)
         var boundary:String
         if (boundary_temp == null)
             boundary=""
         else
             boundary=boundary_temp.value.replace("boundary=","").replace("\"","").replace("\n","")
-        boundary="--"+boundary
-        var text_pieces:List<String> =temp_text.split(boundary)
-//        println(text_pieces.size)
+        var text_pieces:List<String> =body_text.split(boundary)
+//        println("----------------------------------")
+//        println(body_text)
+//        println("----------------------------------")
         for (tp in text_pieces) {
-            temp=contentTypeRegex.find(tp)
+            var temp=contentTypeRegex.find(tp)
             if (temp!=null) {
                 var contentType:String=temp.value.replace("Content-Type: ","").split(";")[0]
+//                println(tp)
 //                println(contentType)
                 when (contentType) {
                     "text/plain"->{
-                        text=tp.split("[\r\n]{3}".toRegex()).drop(1).joinToString("\n\n\n")
+                        val name_temp=contentNameRegex.find(tp)
+                        if (name_temp != null){
+                            var name = name_temp.value.replace("name=\"", "").replace("\"\n", "")
+                            var content=tp.split("\n\n").drop(1).joinToString("\n\n")
+                            File("./"+name).writeText(content)
+                            filelist.add("./"+name)
+//                            println("------------------content-----------------")
+//                            println(content)
+//                            println("------------------content-----------------")
+                        }else{
+                            text+=tp.split("[\r\n]{3}".toRegex()).drop(1).joinToString("\n\n\n")
+                            println(text)
+                        }
                     }
                     "text/html"->{
-//                        println(tp)
-                        html=tp.split("[\r\n]{2}".toRegex()).drop(1).joinToString("\n")
+                        html+=tp.split("[\r\n]{2}".toRegex()).drop(1).joinToString("\n")
+                    }
+                    "multipart/mixed"->{
+                        val emin:Email=process_email(tp,tp,tp,tp)
+                        text+=emin.text
+                        html+=emin.html
+                        filelist.addAll(emin.filelist)
                     }
                     else->{
                         if (contentType.contains("application", ignoreCase = true)||contentType.contains("image", ignoreCase = true)||contentType.contains("video", ignoreCase = true)) {
                             val encoding_temp=contentEncodingRegex.find(tp)
                             val name_temp=contentNameRegex.find(tp)
-                            val boundary_temp=fileBoundary_regex.find(tp)
+                            val boundary_temp=boundaryRegex.find(tp)
                             if (encoding_temp!=null && name_temp != null && boundary_temp!=null){
                                 var encoding=encoding_temp.value.replace("Content-Transfer-Encoding: ","").replace("\n","")
                                 if (encoding=="base64") {
@@ -159,15 +182,33 @@ class IMAPClient(host: String, port: Int) {
                 }
             }
         }
-        println(from)
-        println(to)
-        println(date)
-//        println(boundary)
-        println(text)
-        println(html)
-        println(filelist)
+
+//        println(emaillist)
 //        println(text_pieces[1])
-        return Email(from,to,date,boundary,html,filelist)
+        return Email(from,to,date,text,html,filelist)
+    }
+    fun get_mail(box_name:String,mail_id:String):Email{
+
+
+        select(box_name,false)
+
+        var header_from=fetch(mail_id,"BODY[HEADER.FIELDS (FROM)]",false)
+        var header_to=fetch(mail_id,"BODY[HEADER.FIELDS (TO)]",false)
+        var header_date=fetch(mail_id,"BODY[HEADER.FIELDS (DATE)]",false)
+        var body_text=fetch(mail_id,"RFC822.TEXT",false)
+        return process_email(body_text,header_from,header_to,header_date)
+
+//        boundary="--"+boundary
+//        println(boundary_temp)
+//        println(boundary)
+//        Thread.sleep(1_000)
+//        println("-----------------------------------------------------------")
+//        println(temp_text)
+//
+//        println("-----------------------------------------------------------")
+
+//        println(text_pieces.size)
+
     }
     fun fetch(id:String,whatToGet:String="RFC822.TEXT",doPrint: Boolean =true):String{
         val messageId=sendMessage("FETCH $id $whatToGet",doPrint)
